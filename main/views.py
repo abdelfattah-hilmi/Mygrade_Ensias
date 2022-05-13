be6e4b8e7c4ce635084ceb34ca269278d1d302ec
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-05-13 01:59:04 +0100

    api route /api/grades is working

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from rest_framework.response import Response
 from .models import Grades
 from .serializers import GradeSerializer
@@ -10,9 +10,11 @@ class GradeViewSet(viewsets.ViewSet):
         return Response(serializer.data)
     
     def create(self,request):
-        #/api/product
-        pass
-    
+        serializer= GradeSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
     def read(self,request,pk=None):
         #/api/product/pk
         pass
